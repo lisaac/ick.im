e6cad3a267ba2939319fb5d82861f98e36d9c250
lisaac <lisaac.cn@gmail.com>
2016-01-05 11:08:58 +0800

    speed fix

--- js/typed.js
+++ js/typed.js
@@ -271,7 +271,7 @@
                 // the stopNum actually represents the amount of chars to
                 // keep in the current string. In my case it's 14.
                  if (self.arrayPos == 0){
-                  self.stopNum = 4;
+                  self.stopNum = 5;
                  }
                 //every other time, delete the whole typed string
                  else{
